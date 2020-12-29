004b7cb9e31647c5c2f789dc8f2511c846495b73
aman2025 <2247367564@qq.com>
2020-12-29 16:44:55 +0800

    loading

---
+++ src/components/Loading.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="loading">
+    loading
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading',
+  props: {}
+};
+</script>
+
+<style scoped lang="scss"></style>
