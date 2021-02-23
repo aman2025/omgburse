156b5333b888e14095f3da1962561e42e17fd29e
aman2025 <2247367564@qq.com>
2021-02-23 10:42:35 +0800

    单语言版本：商品详情页，点“order grabbing”,余额不足，点弹出框，yes跳转到充值页面

--- src/views/ProductDetail.vue
+++ src/views/ProductDetail.vue
@@ -80,7 +80,7 @@ import Toast from '@/components/Toast.vue';
 import Loading from '@/components/Loading.vue';
 import Dialog from '@/components/Dialog.vue';
 import { reactive, ref, toRefs } from 'vue';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 import request from '../utils/request';
 
 export default {
@@ -97,7 +97,7 @@ export default {
       message: '',
       isJiazai: false
     });
-    // const routes = useRouter();
+    const routers = useRouter();
     const router = useRoute();
     const isLoad = ref(false); // 设置isLoad=true响应
     const goodsDetail = ref({});
@@ -194,7 +194,9 @@ export default {
     // 确定dialog
     const show = ref(false);
     const content = ref('');
-    const callback = ref(function() {});
+    const callback = ref(function() {
+      routers.push('/Recharge');
+    });
     const onOk = ref(val => {
       show.value = val;
       callback.value();
